e8259a79c74ec6130e40bc5fa52b350f53829058
kishan <kishanpatellpu@gmail.com>
2024-05-06 13:10:20 +0530

    dev1 2nd commit

--- src/main/java/com/batch/Example/SpringbatchApplication.java
+++ src/main/java/com/batch/Example/SpringbatchApplication.java
@@ -9,14 +9,18 @@ public class SpringbatchApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbatchApplication.class, args);
 		System.out.println("hello"
+				+ "hiiii"
+				+ "hooo"
 				+ ""
 				+ ""
 				+ ""
 				+ ""
 				+ ""
-				+ ""
-				+ ""
-				+ "");
+				+ "haaa"
+				
+				+"jelkkonjnfjdsndfnbnjb"
+				);
+		
 	}
 
 }
